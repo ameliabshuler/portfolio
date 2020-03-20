6e06077f4bdd3d0687f44fee15e8dded4d82e477
ameliabshuler <amelia.b.shuler@gmail.com>
2020-03-20 15:40:43 -0400

    new one!

--- js/custom.js
+++ js/custom.js
@@ -1,43 +1,12 @@
-function navHover(id, id) {
-    document.getElementById('homebutton').style.display = 'none';
-    document.getElementById('homebuttonhover').style.display = 'inline-block'; 
-    document.querySelectorAll("#homebutton, #homebuttonhover").style.transition = "all 2s";
+$(document).ready(function(){
+        $("#carouselFashionDesign").carousel();
+   $("#carouselFashionDesign").swiperight(function() {
+      $(this).carousel('prev');
+    });
+   $("#carouselFashionDesign").swipeleft(function() {
+      $(this).carousel('next');
+   });
+});
+function appear(id) {
+    document.getElementById(id).style.visibility = "visible";
 }
-function navRelease(id, id) {
-    document.getElementById('homebuttonhover').style.display = 'none';
-    document.getElementById('homebutton').style.display = 'inline-block';   
-}
-var images = [];
-function preload() {
-    for (var i = 0; i < arguments.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload.arguments[i];
-    }
-}
-
-//-- usage --//
-preload(
-    "assets/header/logo-copy.png",
-    "assets/header/header.png",
-    "assets/carahsoft/carahsoft-homepagecover.jpg",
-    "https://www.linkedin.com/embed/feed/update/urn:li:share:6518908362929815552",
-    "https://www.linkedin.com/embed/feed/update/urn:li:share:6518908362929815552",
-    "assets/carahsoft/carahsoft_background_061019.jpg",
-	"http://www.govexec.com/insights/what-is-cbrs/"
-)
-$('#carousel1flatfit').carousel();
-$('#carousel2flatfit').carousel();
-$('#carousel3flatfit').carousel();
-$('#findARoommateWho').carousel();
-$('#carouselcummins').carousel();
-
-window.onscroll = function() {myHome()};
-
-function myHome() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-document.getElementById("logoHome").className = "afterhome";
-  }
-else {
-document.getElementById("logoHome").className = "beforehome";
-  }}
-$('#photoModal1').modal(options);
