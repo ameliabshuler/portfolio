d06b416eabf5bbd200940b80148a20c6f77b2d0c
Amelia Shuler <33971853+ameliabshuler@users.noreply.github.com>
2020-12-16 21:03:05 -0500

    reput everything

---
+++ js/custom.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});$(document).ready(function(){
+        $("#carouselFashionDesign").carousel();
+   $("#carouselFashionDesign").swiperight(function() {
+      $(this).carousel('prev');
+    });
+   $("#carouselFashionDesign").swipeleft(function() {
+      $(this).carousel('next');
+   });
+});
+$('#cumminsModal1').modal();
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
